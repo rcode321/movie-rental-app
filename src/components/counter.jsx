5bc94c2e25fb0f7486ea613978902f26a9962fe3
Raf <rafaelmendoza004@gmail.com>
2021-07-30 00:21:12 +0800

    added

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,58 +6,58 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
 class Counter extends Component {
-  render() {
-    return (
-      <Container fluid>
-        <Row>
-          <Col xs={2}>
-            <Badge variant={this.getBadgeClasses()}>{this.formatCount()}</Badge>
-          </Col>
+	render() {
+		return (
+			<Container fluid>
+				<Row>
+					<Col xs={2}>
+						<Badge variant={this.getBadgeClasses()}>{this.formatCount()}</Badge>
+					</Col>
 
-          <Col xs={2}>
-            <Button
-              onClick={() => this.props.onIncrement(this.props.counter)}
-              variant="secondary mt-2"
-            >
-              <i className="fa fa-plus" aria-hidden="true"></i>
-            </Button>
-          </Col>
+					<Col xs={2}>
+						<Button
+							onClick={() => this.props.onIncrement(this.props.counter)}
+							variant="secondary mt-2"
+						>
+							<i className="fa fa-plus" aria-hidden="true"></i>
+						</Button>
+					</Col>
 
-          <Col xs={2}>
-            <Button
-              onClick={() => this.props.onDecrement(this.props.counter)}
-              variant="secondary m-2"
-              disabled={
-                this.props.counter.value === 0 ? "secondary disabled m-2" : ""
-              }
-            >
-              <i className="fa fa-minus" aria-hidden="true"></i>
-            </Button>
-          </Col>
+					<Col xs={4}>
+						<Button
+							onClick={() => this.props.onDecrement(this.props.counter)}
+							variant="secondary m-2"
+							disabled={
+								this.props.counter.value === 0 ? "secondary disabled m-2" : ""
+							}
+						>
+							<i className="fa fa-minus" aria-hidden="true"></i>
+						</Button>
+					</Col>
 
-          <Col>
-            <Button
-              onClick={() => this.props.onDelete(this.props.counter.id)}
-              variant="danger mt-2"
-            >
-              <i className="fa fa-times" aria-hidden="true"></i>
-            </Button>
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
+					<Col>
+						<Button
+							onClick={() => this.props.onDelete(this.props.counter.id)}
+							variant="danger mt-2"
+						>
+							<i className="fa fa-times" aria-hidden="true"></i>
+						</Button>
+					</Col>
+				</Row>
+			</Container>
+		);
+	}
 
-  getBadgeClasses() {
-    let classes = "";
-    classes += this.props.counter.value === 0 ? "warning m-2" : "primary m-2";
-    return classes;
-  }
+	getBadgeClasses() {
+		let classes = "";
+		classes += this.props.counter.value === 0 ? "warning m-2" : "primary m-2";
+		return classes;
+	}
 
-  formatCount() {
-    const { value } = this.props.counter;
-    return value === 0 ? "Zero" : value;
-  }
+	formatCount() {
+		const { value } = this.props.counter;
+		return value === 0 ? "Zero" : value;
+	}
 }
 
 export default Counter;
